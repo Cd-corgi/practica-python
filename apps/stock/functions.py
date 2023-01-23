a87daec13a3317c7a68dd17fd99712a79df597d7
cd-corgi <systemjpc@hotmail.com>
2023-01-23 13:51:00 -0500

    prueba con functions

--- apps/stock/functions.py
+++ apps/stock/functions.py
@@ -11,35 +11,37 @@ from .models import *
 
 
 def registrar_OrdenDeCompra(create, orden, ordenDetalle):
-        orden = OrdenDeCompra()
+        ordenNew = OrdenDeCompra()
+        ValidarOrden(orden)
         if create:
-                ValidarOrden(orden)
-
-                num = numeracion.objects.get(id = orden['numeracion'])
-                terceros = Terceros.objects.get(id = orden['proveedor'])
+                # Asignación de variables de las Foreign Keys
+                num        = numeracion.objects.get(id = orden['numeracion'])
+                terceros   = Terceros.objects.get(id = orden['proveedor'])
                 formaPagos = FormaPago.objects.get(id = orden['formaPago'])
-                persona = User.objects.get(username = orden['usuario'])
+                persona    = User.objects.get(username = orden['usuario'])
 
-                orden.numeracion        = num
-                orden.proveedor         = terceros
-                orden.fecha             = orden['fecha']
-                orden.usuario           = persona
-                orden.formaPago         = formaPagos
-                orden.subtotal          = orden['subtotal']
-                orden.iva               = orden['iva']
-                orden.retencion         = orden['retencion']
-                orden.descuento         = orden['descuento']
-                orden.total             = orden['total']
+                # Asugnación de variables en general
+                ordenNew.numeracion        = num
+                ordenNew.proveedor         = terceros
+                ordenNew.fecha             = orden['fecha']
+                ordenNew.usuario           = persona
+                ordenNew.formaPago         = formaPagos
+                ordenNew.subtotal          = orden['subtotal']
+                ordenNew.iva               = orden['iva']
+                ordenNew.retencion         = orden['retencion']
+                ordenNew.descuento         = orden['descuento']
+                ordenNew.total             = orden['total']
 
+                # Atomic que procesa los cambios y añade mas valoes a OrdenDetalle
                 with transaction.atomic():
-                        orden.save()
+                        ordenNew.save()
                         detalle = []
+                        # for que itera todo los campos de OrdenDetalle
                         for item in ordenDetalle:
-                                d = OrdenDetalle()
-
-                                p = item['producto']
-                                prod = Productos.object.get(id = p.id)
-                                d.orden       = orden
+                                d             = OrdenDetalle()
+                                p             = item['producto']
+                                prod          = Productos.object.get(id = p.id)
+                                d.orden       = ordenNew
                                 d.producto    = prod
                                 d.cantidad    = item['cantidad']
                                 d.valorUnidad = item['valorUnidad']   
@@ -47,10 +49,66 @@ def registrar_OrdenDeCompra(create, orden, ordenDetalle):
                                         d.descuento   = item['descuento']
                                 if item['iva'] == None or item['iva'] == '':
                                         d.iva   = item['iva']
+                                # Añade los datos al arreglo "Detalle"
                                 detalle.append(d)
+                        # Hace una inserción masiva a OrdenDetalle                                
                         OrdenDetalle.objects.bulk_create(detalle)
-                return orden
+                return ordenNew
+        else:
+                # Consulta de la ID del modelo de OrdenDetalle con OrdenDeCompra
+                try:
+                     ordenNew = OrdenDeCompra.objects.get(id = orden['id'])
+                except ordenNew.ObjectDoesNotExist as e:
+                        raise serializers.ValidationError('La id por actualizar no existe')
+                
+                # Asignación de variables de las Foreign Keys
+                num        = numeracion.objects.get(id = orden['numeracion'])
+                terceros   = Terceros.objects.get(id = orden['proveedor'])
+                formaPagos = FormaPago.objects.get(id = orden['formaPago'])
+                persona    = User.objects.get(username = orden['usuario'])
 
+                # Asugnación de variables en general
+                ordenNew.numeracion        = num
+                ordenNew.proveedor         = terceros
+                ordenNew.fecha             = orden['fecha']
+                ordenNew.usuario           = persona
+                ordenNew.formaPago         = formaPagos
+                ordenNew.subtotal          = orden['subtotal']
+                ordenNew.iva               = orden['iva']
+                ordenNew.retencion         = orden['retencion']
+                ordenNew.descuento         = orden['descuento']
+                ordenNew.total             = orden['total']
+
+                # Atomic que procesa los cambios y añade mas valoes a OrdenDetalle
+                with transaction.atomic():
+                        ordenNew.save()
+                        ordenD = OrdenDetalle.objects.filter(orden = orden['id'])
+                        ordenD.delete()
+                        detalle = []
+                        # for que itera todo los campos de OrdenDetalle
+                        for item in ordenDetalle:
+                                d             = OrdenDetalle()
+                                p             = item['producto']
+                                prod          = Productos.object.get(id = p.id)
+                                d.orden       = ordenNew
+                                d.producto    = prod
+                                d.cantidad    = item['cantidad']
+                                d.valorUnidad = item['valorUnidad']   
+                                if item['descuento'] == None or item['descuento'] == '':
+                                   d.descuento   = 0
+                                else: 
+                                   d.descuento   = item['descuento']   
+                                if item['iva'] == None or item['iva'] == '':
+                                   d.iva   = 0
+                                else:
+                                   d.iva   = item['iva']
+                                # Añade los datos al arreglo "Detalle"
+                                detalle.append(d)
+                        # Hace una inserción masiva a OrdenDetalle        
+                        OrdenDetalle.objects.bulk_create(detalle)
+                return ordenNew
+
+                
 
 
 def getProductos_SinStock():
